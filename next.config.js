9573a897cb182f1d93aefa7cd666b68b4c573dd9
vineetpandey39 <vineetpandey39@gmail.com>
2026-06-01 23:06:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
+};
 module.exports = nextConfig;
